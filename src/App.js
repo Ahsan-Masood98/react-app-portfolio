c900ca831e555de83e92c97dd39c14c303e2ee6e
Ahsan-Masood98 <ahsan.masood9@outlook.com>
2022-03-27 20:42:43 +0500

    Intro to StyledComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//GlobalStyled Component
+import GlobalStyle from "./components/GlobalStyled";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
